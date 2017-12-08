48fb8f0d862b9cd0157a87477c9ae2377d502d07
Pei1133 <pei.7969pei.7969@gmail.com>
2017-12-08 11:23:37 +0800

    ListsViewController add

---
+++ Journal/Journal.swift
@@ -0,0 +1,26 @@
+//
+//  Journal.swift
+//  Journal
+//
+//  Created by 黃珮鈞 on 2017/12/8.
+//  Copyright © 2017年 黃珮鈞. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class Journal {
+    var title: String = ""
+    var text: String = ""
+    var date: String = ""
+    let ref: DatabaseReference!
+
+    init(snapshot: DataSnapshot) {
+        self.ref = snapshot.ref
+        if let value = snapshot.value as? [String: Any] {
+            self.title = value["title"] as! String
+            self.text = value["content"] as! String
+            self.date = value["publish date"] as! String
+        }
+    }
+}
---
+++ Journal/ListsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListsViewController.swift
+//  Journal
+//
+//  Created by 黃珮鈞 on 2017/12/8.
+//  Copyright © 2017年 黃珮鈞. All rights reserved.
+//
+
+import UIKit
+
+class ListsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
