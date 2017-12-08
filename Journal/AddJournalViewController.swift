2e79f7cf63593cae7484524a52e043793c209564
Pei1133 <pei.7969pei.7969@gmail.com>
2017-12-08 15:21:26 +0800

    ImagePicker

--- Journal/AddJournalViewController.swift
+++ Journal/AddJournalViewController.swift
@@ -9,18 +9,24 @@
 import UIKit
 import Firebase
 
-class AddJournalViewController: UIViewController {
+class AddJournalViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var journalTextField: UITextView!
-    @IBOutlet weak var addImageView: UIImageView!
-   
+    @IBOutlet weak var pickUpImageButtom: UIButton!
+
+    @IBAction func tapButton(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     @IBAction func goSave(_ sender: Any) {
 
         let ref = Database.database().reference()
         let journalRef = ref.child("journals").childByAutoId()
-//        let childAutoID = journalRef.key
-
 
         guard let title = self.titleTextField.text, let content = self.journalTextField.text else{
             print("Form is not valid")
@@ -34,22 +40,12 @@ class AddJournalViewController: UIViewController {
             ] as [String : Any]
         
         journalRef.setValue(value)
-        
-//          FirebaseRef.databaseProducts.child("\(product.id.rawValue)").updateChildValues(value)
-        
-//        // store journal ids in user
-//        let userArticlesReference = ref.child("users").child(uid).child("articles").childByAutoId()
-//        userArticlesReference.setValue(childAutoID)
-        
-        self.navigationController?.popViewController(animated: true)
-        
+
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,7 +53,34 @@ class AddJournalViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+//    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+//        return "確認刪除"
+//    }
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == UITableViewCellEditingStyle.delete {
+////            self.allNames[indexPath.section]?.remove(at: indexPath.row)
+//            tableView.setEditing(false, animated: true)
+//        }
+//
+//        else if editingStyle == UITableViewCellEditingStyle.insert
+//        {
+////            allNames[indexPath.section]?.insert("插入的", at: indexPath.row)
+//            tableView.setEditing(false, animated: true)
+//        }
+//
+//        tableView.reloadData()
+//    }
 
+    //    MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        if let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = chosenImage
+            imageView.contentMode = .scaleAspectFit
+        }
+        dismiss(animated:true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ Journal/EditJournalViewController.swift
@@ -0,0 +1,46 @@
+//
+//  EditJournalViewController.swift
+//  Journal
+//
+//  Created by 黃珮鈞 on 2017/12/8.
+//  Copyright © 2017年 黃珮鈞. All rights reserved.
+//
+
+import UIKit
+
+class EditJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+//    for product in products {
+//    let name = product.name
+//    let price = product.price
+//    let urlString = "http://52.198.40.72/patissier/products/\(product.id.rawValue)/preview.jpg"
+//    let value: [String: Any] = ["name": name, "price": price, "imageURL": urlString]
+//    FirebaseRef.databaseProducts.child("\(product.id.rawValue)").updateChildValues(value)
+//    
+//    self.navigationController?.popViewController(animated: true)
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
